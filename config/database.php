e7eebc70a1df4a7fb8d9c4d97d91a96fb71c7d21
leek <lsj8380880@hotmail.com>
2019-01-04 14:21:34 +0800

    by leek

--- config/database.php
+++ config/database.php
@@ -110,9 +110,9 @@ return [
         'client' => 'predis',
 
         'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
+            'host' => env('SESS_REDIS_HOST', '127.0.0.1'),
+            'password' => env('SESS_REDIS_PASSWORD', null),
+            'port' => env('SESS_REDIS_PORT', 6379),
             'database' => 0,
         ],
 
